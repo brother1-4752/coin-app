beab02d155ec91ce813a7c2f9d4397202e4c69fd
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-04-08 05:03:54 +0900

    update : deploy.sh 수정, vite.config 'base' 추가

--- deploy.sh
+++ deploy.sh
@@ -10,20 +10,6 @@ git init
 
 git add .
 
-commitMessage=$(git log -1 --pretty=%B)
-# 기존의 커밋 메시지를 가져옴
-
-if [[ $commitMessage =~ ^deploy\ :\ v([0-9]+)$ ]]; then
-  # 만약 기존의 커밋 메시지가 "deploy : vX" 형식이라면,
-  versionNumber="${BASH_REMATCH[1]}"
-  # 배포한 버전 정보(vX)를 추출
-  newVersionMessage="deploy : v$((versionNumber + 1))"
-  # 새로운 커밋 메시지를 만듦
-else
-  newVersionMessage="deploy : v1"
-  # 만약 이전에 배포한 적이 없다면 새로운 커밋 메시지를 만듦
-fi
-
-git commit -m "$newVersionMessage"
+git commit -m "배포 : 깃허브페이지"
 
 git push --force "git@github.com:brother1-4752/coin-app.git" master:gh-pages
